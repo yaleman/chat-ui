ee917ac9322038d0294c00a07e73fe840a08978e
James Hodgkinson <james@terminaloutcomes.com>
2024-03-05 14:57:11 +1000

    fixing all the messages

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ import os
 from uuid import uuid4
 
 import pytest
-from chat_ui.models import WebSocketMessage
+from chat_ui.models import JobStatus, RequestType, WebSocketMessage
 
 os.environ["CHATUI_BACKEND_URL"] = "test"
 
@@ -13,3 +13,15 @@ def test_websocketmessage() -> None:
         WebSocketMessage.model_validate({"userid": "foobar", "message": "jobs"})
     with pytest.raises(ValueError):
         WebSocketMessage.model_validate({"userid": str(uuid4()), "message": "test"})
+
+
+def test_jobstatus() -> None:
+    with pytest.raises(ValueError):
+        JobStatus("foobar")
+    JobStatus("created")
+
+
+def test_requestype() -> None:
+    with pytest.raises(ValueError):
+        RequestType("foobar")
+    RequestType("plain")
